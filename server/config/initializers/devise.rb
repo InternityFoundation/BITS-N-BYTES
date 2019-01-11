17e745aa93f1c8a2287f948d246c47bddeebcbe6
SHLOK WALIA <virgoshlok@gmail.com>
2019-01-12 00:41:42 +0530

    Added google login

--- server/config/initializers/devise.rb
+++ server/config/initializers/devise.rb
@@ -3,6 +3,13 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.omniauth :google_oauth2, '950846929065-vn4iigq865e6anp26d34bjjrmg87g2nc.apps.googleusercontent.com', '9bywe0DIvLuCIf6u8kNgIWaF', {
+      scope: 'userinfo.email, userinfo.profile',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+      image_size: 50
+  }
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
